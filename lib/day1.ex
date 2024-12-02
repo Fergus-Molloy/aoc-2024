c9342c422e142de57628002ef87938921c2bc8a1
Fergus Molloy <fergus@molloy.xyz>
2024-12-02 02:26:44 +0000

    day 1 pt 2

--- lib/day1.ex
+++ lib/day1.ex
@@ -2,16 +2,21 @@ defmodule Day1 do
   def pt1(inp) do
     {ls, rs} =
       inp
-      |> String.trim()
-      |> String.split("\n")
-      |> Enum.map(&parseLine/1)
-      |> Enum.unzip()
+      |> parse()
 
     sorted_ls = Enum.sort(ls)
     sorted_rs = Enum.sort(rs)
 
     Enum.zip_with(sorted_ls, sorted_rs, fn a, b -> if a > b, do: a - b, else: b - a end)
     |> Enum.sum()
+  end
+
+  def parse(inp) do
+    inp
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&parseLine/1)
+    |> Enum.unzip()
   end
 
   def parseLine(line) do
@@ -22,5 +27,11 @@ defmodule Day1 do
       |> Enum.map(&elem(&1, 0))
 
     {ls, rs}
+  end
+
+  def pt2(inp) do
+    {ls, rs} = inp |> parse()
+
+    ls |> Enum.map(fn l -> l * Enum.count(rs, fn x -> x == l end) end) |> Enum.sum()
   end
 end
--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -5,4 +5,9 @@ defmodule AocTest do
     {:ok, file} = File.read("./inputs/1")
     assert Day1.pt1(file) == 2_164_381
   end
+
+  test "Day 1 pt2" do
+    {:ok, file} = File.read("./inputs/1")
+    assert Day1.pt2(file) == 20_719_933
+  end
 end
